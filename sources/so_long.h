40b0f92aaf49958504452b002a684ced561a4953
maolivei <maolivei@student.42sp.org.br>
2022-05-25 07:28:37 +0200

    Remove unused declarations

--- sources/so_long.h
+++ sources/so_long.h
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 10:24:18 by maolivei          #+#    #+#             */
-/*   Updated: 2022/05/25 07:25:23 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/05/25 07:28:00 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,6 @@
 # define WHITE "\033[0;37m"
 # define RESET "\033[0m"
 
-typedef struct s_img {
-	void	*img_ptr;
-	char	*address;
-	int		bpp;
-	int		line_len;
-	int		endian;
-}	t_img;
-
 typedef struct s_data {
 	void	*mlx_ptr;
 	void	*win_ptr;
@@ -61,7 +53,6 @@ typedef struct s_data {
 
 int		ft_validate_map(t_data *data);
 int		ft_close_game(int keysym, t_data *data);
-void	ft_img_pix_put(t_img *image, int x, int y, int color);
 int		ft_render(t_data *data);
 
 #endif /* SO_LONG_H */
