9cab14e569e269d9796111b97f36efa446d32c92
maolivei <maolivei@student.42sp.org.br>
2022-05-27 19:39:12 +0200

    Reduce code duplication in player motion

--- sources/player_movement.c
+++ sources/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 22:45:26 by maolivei          #+#    #+#             */
-/*   Updated: 2022/05/27 01:33:20 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/05/27 19:03:13 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,73 +15,53 @@
 static int	ft_move_player_up(t_data *data)
 {
 	if (data->map[data->player_y - 1][data->player_x] == '1')
-		return (0);
+		return (FALSE);
 	else if (data->map[data->player_y - 1][data->player_x] == 'C')
 		data->collected++;
 	else if (data->map[data->player_y - 1][data->player_x] == 'E')
-	{
-		if (data->collected == data->c_count)
-			return (ft_end_game(data));
-		else
-			return (0);
-	}
+		return (ft_end_game(data));
 	data->map[data->player_y][data->player_x] = '0';
 	data->map[--(data->player_y)][data->player_x] = 'P';
-	return (1);
+	return (TRUE);
 }
 
 static int	ft_move_player_left(t_data *data)
 {
 	if (data->map[data->player_y][data->player_x - 1] == '1')
-		return (0);
+		return (FALSE);
 	else if (data->map[data->player_y][data->player_x - 1] == 'C')
 		data->collected++;
 	else if (data->map[data->player_y][data->player_x - 1] == 'E')
-	{
-		if (data->collected == data->c_count)
-			return (ft_end_game(data));
-		else
-			return (0);
-	}
+		return (ft_end_game(data));
 	data->map[data->player_y][data->player_x] = '0';
 	data->map[data->player_y][--(data->player_x)] = 'P';
-	return (1);
+	return (TRUE);
 }
 
 static int	ft_move_player_down(t_data *data)
 {
 	if (data->map[data->player_y + 1][data->player_x] == '1')
-		return (0);
+		return (FALSE);
 	else if (data->map[data->player_y + 1][data->player_x] == 'C')
 		data->collected++;
 	else if (data->map[data->player_y + 1][data->player_x] == 'E')
-	{
-		if (data->collected == data->c_count)
-			return (ft_end_game(data));
-		else
-			return (0);
-	}
+		return (ft_end_game(data));
 	data->map[data->player_y][data->player_x] = '0';
 	data->map[++(data->player_y)][data->player_x] = 'P';
-	return (1);
+	return (TRUE);
 }
 
 static int	ft_move_player_right(t_data *data)
 {
 	if (data->map[data->player_y][data->player_x + 1] == '1')
-		return (0);
+		return (FALSE);
 	else if (data->map[data->player_y][data->player_x + 1] == 'C')
 		data->collected++;
 	else if (data->map[data->player_y][data->player_x + 1] == 'E')
-	{
-		if (data->collected == data->c_count)
-			return (ft_end_game(data));
-		else
-			return (0);
-	}
+		return (ft_end_game(data));
 	data->map[data->player_y][data->player_x] = '0';
 	data->map[data->player_y][++(data->player_x)] = 'P';
-	return (1);
+	return (TRUE);
 }
 
 void	ft_move_player(int keysym, t_data *data)
