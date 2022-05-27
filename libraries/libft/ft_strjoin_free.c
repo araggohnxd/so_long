33a9eca4559ec89a36255453b24627d51fff2575
maolivei <maolivei@student.42sp.org.br>
2022-05-27 19:40:27 +0200

    Reduce code duplication by aliasing strjoin

--- libraries/libft/ft_strjoin_free.c
+++ libraries/libft/ft_strjoin_free.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 12:18:13 by maolivei          #+#    #+#             */
-/*   Updated: 2022/05/09 20:17:35 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/05/27 18:34:31 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,8 @@
 char	*ft_strjoin_free(char *s1, char *s2)
 {
 	char	*str;
-	size_t	s1_len;
-	size_t	s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = malloc(s1_len + s2_len + 1);
-	if (str == NULL)
-		return (NULL);
-	ft_strlcpy(str, s1, s1_len + 1);
-	ft_strlcat(str, s2, s1_len + s2_len + 1);
+	str = ft_strjoin(s1, s2);
 	free(s1);
 	free(s2);
 	return (str);
